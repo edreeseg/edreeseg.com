44724b3d2afdf95c1d5e6885ec177986993c1128
edreeseg <edward.reeseg@gmail.com>
2019-08-06 16:38:52 -0400

    Basic touch functionality for accordion

--- src/components/PortfolioPreview/ProjectCarousel.js
+++ src/components/PortfolioPreview/ProjectCarousel.js
@@ -1,11 +1,36 @@
 import React from 'react';
 
 class ProjectCarousel extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            touchStart: { x: null, y: null },
+            lastMove: { x: null, y: null },
+        };
+    }
+    registerTouchStart = ev => {
+        this.setState({
+            touchStart: { x: ev.touches[0].clientX, y: ev.touches[0].clientY },
+        });
+    }
+    registerTouchMove = ev => {
+        this.setState({ lastMove: { x: ev.touches[0].clientX, y: ev.touches[0].clientY }});
+    }
+    registerTouchEnd = ev => {
+        const differenceX = this.state.touchStart.x - this.state.lastMove.x;
+        const differenceY = Math.abs(this.state.touchStart.y - this.state.lastMove.y);
+        if (differenceY > Math.abs(differenceX)) return;
+        if (differenceX > 0) return this.props.changeSelected(true)();
+        else return this.props.changeSelected(false)();
+    }
     render(){
-        console.log(this.props.changeSelected);
         const current = this.props.projects[this.props.current];
         return (
-            <section className="project-select">
+            <section className="project-select" 
+                onTouchStart={this.registerTouchStart}
+                onTouchMove={this.registerTouchMove}
+                onTouchEnd={this.registerTouchEnd}    
+            >
                 <div className="left-outline" />
                 <div className="arrow-left" onClick={this.props.changeSelected(false)} />
                 <img src={current.img} alt={current.name} className="project-slide" />
