447321e13b82ef69dc1be654c6e6650d205cff5c
edreeseg <edward.reeseg@gmail.com>
2019-10-10 14:40:22 -0400

    About section prototype complete

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Link } from 'react-scroll';
+import { faLinkedin, faGithubSquare, faTwitterSquare } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './About.scss';
 
 function About() {
@@ -11,6 +13,18 @@ function About() {
             </section>
             <section className="about-synopsis">
                 <p>I'm Ed!  I'm a full-stack software engineer looking to build great things with great people.</p>
+                <p>Connect with me on social media!  I look forward to hearing from you.</p>
+            </section>
+            <section className="about-social-media">
+                <a href="https://www.linkedin.com/in/ed-reeseg-30a8b284/" target="_blank" rel="noopener noreferrer">
+                    <FontAwesomeIcon icon={faLinkedin} size="3x" color="#4875B4" />
+                </a>
+                <a href="https://github.com/edreeseg" target="_blank" rel="noopener noreferrer">
+                    <FontAwesomeIcon icon={faGithubSquare} size="3x" color="#040817" />
+                </a>
+                <a href="https://twitter.com/edreeseg" target="_blank" rel="noopener noreferrer">
+                    <FontAwesomeIcon icon={faTwitterSquare} size="3x" color="#33CCFF" />
+                </a>
             </section>
         </section>
     );
